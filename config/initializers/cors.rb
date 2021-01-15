2c9aad8a55f9ccaf048bb96eb00baaa7790596a3
Harrison Malone <harrison.malone2@gmail.com>
2021-01-15 15:16:29 +1100

    added new url to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'harrisonsmusic.netlify.app'
+    origins 'localhost:8080', 'harrisonsmusic.netlify.app', 'hahrmuhnee.dev'
 
     resource '*',
       headers: :any,
